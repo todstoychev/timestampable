816af8f385ebc5dd60cd93fcb9ecaac66d90e89c
todstoychev <todstoychev@gmail.com>
2019-08-03 10:50:46 +0300

    Added subscriber

--- src/TimestampableBundle/DependencyInjection/TodstoychevTimestampableExtension.php
+++ src/TimestampableBundle/DependencyInjection/TodstoychevTimestampableExtension.php
@@ -2,7 +2,9 @@
 
 namespace Todstoychev\TimestampableBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
@@ -17,6 +19,10 @@ class TodstoychevTimestampableExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-
+        $loader = new XmlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../../../config')
+        );
+        $loader->load('services.xml');
     }
 }
---
+++ src/TimestampableBundle/Subscriber/TimestampableEventSubscriber.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Todstoychev\TimestampableBundle\Subscriber;
+
+use DateTime;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
+use Doctrine\ORM\Events;
+use Todstoychev\TimestampableBundle\Entity\TimestampableInterface;
+
+/**
+ * EntityListener
+ *
+ * @package Todstoychev\TimestampableBundle\Listener
+ * @author  Todor Todorov <todstoychev@gmail.com>
+ **/
+class TimestampableEventSubscriber implements EventSubscriber
+{
+    /**
+     * Returns an array of events this subscriber wants to listen to.
+     *
+     * @return array
+     */
+    public function getSubscribedEvents()
+    {
+        return [
+            Events::postPersist,
+            Events::postUpdate,
+        ];
+    }
+
+    public function postUpdate(LifecycleEventArgs $args)
+    {
+        $this->index($args);
+    }
+
+    public function postPersist(LifecycleEventArgs $args)
+    {
+        $this->index($args);
+    }
+
+    public function index(LifecycleEventArgs $args)
+    {
+        $entity = $args->getObject();
+
+        if ($entity instanceof TimestampableInterface) {
+            $entity->setUpdatedAt(new DateTime('now'));
+
+            if (null === $entity->getCreatedAt()) {
+                $entity->setCreatedAt(new DateTime('now'));
+            }
+        }
+    }
+}
